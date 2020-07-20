e6f60051762021234849c22b2accd121e1280aae
arneesh <arneeshaima@gmail.com>
2020-07-20 20:08:47 +0530

    created redux action types enum reducer

---
+++ src/actions/index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+// import 'redux'
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+interface ToDo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+    return async (dispatch: Dispatch) => {
+        const response = await axios.get<ToDo[]>(url);
+
+        dispatch({
+            type: ActionTypes.fetchTodos,
+            payload: response.data
+        })
+    }
+};
