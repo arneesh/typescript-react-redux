56ebc796070909c465f30cb9eb7795192bfb5224
arneesh <arneeshaima@gmail.com>
2020-07-20 21:01:10 +0530

    connected the entire app to redux and created first App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends React.Component {
+interface AppProps {
+    todos: Todo[];
+    fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+    
+    onButtonClick = (): void => {
+        this.props.fetchTodos();
+    }
+
+    renderList(): JSX.Element[] {
+
+        return this.props.todos.map((todo: Todo) => {
+            return <div key={todo.id}>{todo.title} </div>
+        });
+    }
+
     render() {
-        return <div>Hello World</div>
+        return (<div><button onClick={this.onButtonClick}>Fetch Data</button>
+        <div>{this.renderList()}</div>
+        </div>)
     }
 }
+
+const mapStateToProps = ({todos}: StoreState): { todos: Todo[]} => {
+    return { todos: todos }
+};
+
+export const App = connect(
+    mapStateToProps, { fetchTodos }
+)(_App);
