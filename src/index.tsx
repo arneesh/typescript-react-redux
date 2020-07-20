0ca95c9ce1fad5dbaf00655ded89519b2a22f6a6
arneesh <arneeshaima@gmail.com>
2020-07-20 18:36:02 +0530

    added interfaces for component props and state

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+interface AppProps {
+    color?: string;
+}
 
-class App extends React.Component {
+// interface AppState {
+//     counter: number;
+// }
+
+class App extends React.Component<AppProps> {
+
+    state = { counter: 0 };
+
+    // Second Approach - Pass AppState interface to Component Generic
+    constructor(props: AppProps) {
+        super(props);
+        this.state = { counter: 0};
+    }
+
+    onIncrement = (): void => {
+        this.setState({counter: this.state.counter + 1});
+    }
+
+    onDecrement = (): void => {
+        this.setState({ counter: this.state.counter - 1})
+    }
+
+
 
     render() {
-        return <div>Hi There</div>
+    return (
+        <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+        </div>
+        
+    )
     }
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
